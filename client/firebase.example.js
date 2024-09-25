db4ac178a7bfd7326fcd2b9fafd577643a3e6fb8
ImashNipun <it21034404@my.sliit.lk>
2024-09-25 16:15:12 +0530

    Add a comment

--- client/firebase.example.js
+++ client/firebase.example.js
@@ -1,5 +1,8 @@
 import { initializeApp } from "firebase/app";
 
+//you can download this kind of file for your project
+//firebase console => select your project => project settings
+
 //Update firebaseConfig with your details
 const firebaseConfig = {
   apiKey: "apiKey",
