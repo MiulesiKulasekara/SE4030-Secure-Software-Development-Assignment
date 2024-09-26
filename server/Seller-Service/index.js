274cef17d15f2bdb4975fb29bacf47df7c672201
Ravishka2000 <gamageravishkadulshan2000@gmail.com>
2024-09-26 19:52:06 +0530

    solve information exposure vulanarability in seller service

--- server/Seller-Service/index.js
+++ server/Seller-Service/index.js
@@ -5,27 +5,49 @@ import dotenv from "dotenv";
 import morgan from "morgan";
 import SellerRoutes from "./routes/SellerRoutes.js";
 import cookieParser from "cookie-parser";
+import helmet from "helmet"; 
+import rateLimit from 'express-rate-limit';
 
 dotenv.config();
 
 const app = express();
 const PARAMS = {
-    useNewUrlParser: true, 
-    useUnifiedTopology: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 };
-
 const URI = process.env.MONGOOSE_URI;
 const PORT = process.env.PORT || 5000;
 
-app.use(morgan('dev'));
+// Middleware
+app.use(morgan("dev"));
 app.use(express.json());
-app.use(cors());
 app.use(cookieParser());
+app.use(helmet());
+app.disable("x-powered-by");
 
+// CORS Configuration
+app.use(cors({
+  origin: process.env.ALLOWED_ORIGINS.split(','), // Ensure this is set in your .env file
+  credentials: true,
+}));
+
+// Rate Limiting
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+});
+app.use(limiter);
+
+// Routes
 app.use("/api/seller", SellerRoutes);
 
+// MongoDB Connection
 mongoose.set("strictQuery", false);
-mongoose.connect(URI, PARAMS)
-    .then(() => app.listen(PORT, 
-        () => console.info(`Seller Service is running on PORT ${PORT} 🔥`)))
-    .catch((err) => console.error(err.message));
+mongoose
+  .connect(URI, PARAMS)
+  .then(() =>
+    app.listen(PORT, () =>
+      console.info(`Seller Service is running on PORT ${PORT} 🔥`)
+    )
+  )
+  .catch((err) => console.error(err.message));
